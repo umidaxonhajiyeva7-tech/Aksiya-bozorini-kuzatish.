037677a8ed3b6cfac12f5aad7d0ef4e96cc42bad
umidaxonhajiyeva7-tech <umidaxonhajiyeva7@gmail.com>
2026-05-16 12:12:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+data = {
+    "Kun": ["Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma"],
+    "Narx": [120, 125, 123, 130, 128]
+}
+
+df = pd.DataFrame(data)
+
+print(df)
+
+plt.plot(df["Kun"], df["Narx"], marker='o')
+
+plt.title("Aksiya bozori")
+plt.xlabel("Kun")
+plt.ylabel("Narx")
+
+plt.show()
